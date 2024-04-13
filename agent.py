minor errors like softmax function corrected

agent.py
@@ -3,13 +3,15 @@ import random
 import math
 
 class Agent:
-    def __init__(self, num_actions,num_cumul_actions, learning_rate, beta, epsilon, variable_cost, fixed_cost,memory=False):
+    def __init__(self, num_actions,num_cumul_actions, learning_rate,gamma, 
+                 beta, epsilon, variable_cost, fixed_cost,memory=False):
         self.num_actions = num_actions
         self.num_cumul_actions = num_cumul_actions
         self.q = 0
         self.state = [self.q, 0]
         self.learning_rate = learning_rate
         self.beta = beta
+        self.gamma = gamma
         self.epsilon = epsilon
         self.w = variable_cost
         self.f = fixed_cost
@@ -21,25 +23,37 @@ class Agent:
             self.qtable = np.zeros((1, 1, num_actions))
 
     def reset(self):
-        if self.memory:
+        # the following makes no sense if they should learn:
+        '''if self.memory:
             self.qtable = np.zeros((self.num_actions, self.num_cumul_actions, self.num_actions))
         else:
-            self.qtable = np.zeros((1, 1, self.num_actions))
+            self.qtable = np.zeros((1, 1, self.num_actions))'''
         self.q = 0
         self.state = [0, 0]
 
     def softmax(self):
-        total_sum = sum([np.exp(self.qtable[self.state[0], self.state[1], a]/self.beta) for a in range(self.num_actions)])
 
-        logit = lambda a: np.exp(self.qtable[self.state[0], self.state[1], a]/self.beta) / total_sum
-        probs = [logit(action) for action in range(0,self.num_actions)]
+        total_sum = sum([np.exp(self.qtable[self.state[0], self.state[1], q]/self.beta) for q in range(self.num_actions)])
+        
+        # handle to high values which cause math errors:
+        if not total_sum < 10000000000:
+            q_list = self.qtable[self.state[0], self.state[1]]
+            idx = np.argmax(q_list)
+            probs = [0]*len(q_list)
+            probs[idx] = 1
+            #print('---------------- Runtime error solved ----------------------')
+            return probs
+        
+        soft = lambda q: np.exp(self.qtable[self.state[0], self.state[1], q]/self.beta) / total_sum
+        probs = [soft(q) for q in range(self.num_actions)]
         return probs
 
     def get_profit(self,price):
         return price*self.q - self.w*self.q - self.f
 
-    def select_action(self):
-
+    def select_action(self,beta):
+        
+        self.beta = beta
         probs = self.softmax()
 
         collect = 0
@@ -50,6 +64,8 @@ class Agent:
 
         r = random.uniform(0, 1)
 
+        action = 0
+
         for i in range(0, self.num_actions):
             if r < p[i]:
                 action = i
@@ -58,23 +74,12 @@ class Agent:
         self.q = action
 
         return action
-               
-        # probs = self.softmax()
-        # action = np.random.choice(range(self.num_actions), p=probs)
-        
-        # # Explore
-        # if np.random.rand() < self.epsilon:
-        #     action = np.random.randint(self.num_actions)
-        # # Exploit
-        # else:
-        #     action = np.argmax(self.qtable[0])
-        # return action
 
     def decrease_epsilon(self, episode, decay_rate=0.99, min_epsilon=0.01):
         self.epsilon = max(min_epsilon, np.exp(-decay_rate*episode))
 
-    def update_qtable(self, action, reward,sum_qs):
-        self.qtable[self.state[0], self.state[1], action] = (1 - self.learning_rate) * self.qtable[self.state[0], self.state[1], action] + self.learning_rate * reward
+    def update_qtable(self, reward, sum_qs):
+        self.qtable[self.state[0], self.state[1], self.q] = (1 - self.learning_rate) * self.qtable[self.state[0], self.state[1], self.q] + self.learning_rate * (reward +self.gamma*max(self.qtable[self.state[0], self.state[1]]))
         if self.memory:
-            self.state[0] = action
+            self.state[0] = self.q
             self.state[1] = sum_qs

test.py
@@ -1,28 +1,30 @@
 import numpy as np
 import environs
 import agent
+from tqdm import tqdm
 
 np.set_printoptions(precision=2)
 
-num_agents = 2 # in paper: [2 , 3, 4 , 5 , 6]
+num_agents = 6 # in paper: [2 , 3, 4 , 5 , 6]
 
-num_actions = 4 # in paper: 40
+num_actions = 40 # in paper: 40
 
 # cumulative actions: with memory: num_agents*num_actions, without memory: 1
-num_cumulative_action = num_agents*num_actions 
+num_cumulative_action = num_agents*num_actions # 1 or num_agents*num_actions
 if num_cumulative_action>1:
     memory = True
 else:
     memory = False
 
-demand_quantity = 4 # in paper it is denoted with u=40
+demand_quantity = 40 # in paper it is denoted with u=40
 demand_factor = 1 # in paper it is denoted with v=1
 
-variable_cost = 4 # the variable cost w = 4
+variable_cost = 2 # the variable cost w = 4
 fixed_cost = 0 # in the paper we have f = 0
 
 learning_rate = 0.5 # in paper: [ 0.05 , 0.25 , 0.5 , 1 ]
 beta = 1000 # for chosing like in the paper
+beta_decay = 0.999
 # In paper decay by factor 0.99999 each time step, but computationally 0.999 better
 
 # with memory means we have states different states for each agent:
@@ -37,16 +39,19 @@ epsilon = 0.1 # in paper: not used, but for less computational effort keep that 
 
 # unresolved: Read!
 # TODO: Do they in the paper have several episodes so reset environment several times?
-num_episodes = 1000
-max_steps = 1000
+num_episodes = 900     #1000 in paper
+max_steps = 10        #1000 in paper
 
 env = environs.Cournot(num_agents,demand_quantity,demand_factor)
 
-agents = [agent.Agent(num_actions, num_cumulative_action,
-                      learning_rate, beta, epsilon, variable_cost, fixed_cost,memory)
+agents = [agent.Agent(num_actions, num_cumulative_action,learning_rate,
+                      gamma, beta, epsilon, variable_cost, fixed_cost,memory)
                       for _ in range(num_agents)]
 
-for episode in range(num_episodes):
+joint_profits = []
+joint_quantities = []
+
+for episode in tqdm(range(num_episodes)):
 
     # reset the environment
     state, info = env.reset()
@@ -55,29 +60,62 @@ for episode in range(num_episodes):
     sum_qs = 0
 
     for s in range(max_steps):
-        actions = [agent.select_action() for agent in agents]
+        actions = [agent.select_action(beta) for agent in agents]
         
         # take action and observe reward
         sum_qs, rewards = env.step(actions, agents)
 
+        #results:
+        joint_profits.append(sum(rewards))
+        joint_quantities.append(sum_qs)
+
         # Q-learning algorithm
         for agent_idx, reward in enumerate(rewards):
-            agents[agent_idx].update_qtable(actions[agent_idx], reward,sum_qs)
+            agents[agent_idx].update_qtable(reward,sum_qs)
 
-        beta = beta*0.99
-
+        beta = beta*beta_decay
     
     # for agent in agents:
     #     agent.decrease_epsilon(episode)
 
-print('Beta is:' +str(beta))
+print(f'beta after final episode {num_episodes} is: {beta}')
 
 print(f"Training completed over {num_episodes} episodes")
-for i, agent in enumerate(agents):
-    print(f"Q-values for agent {i}:")
-    for a in range(num_actions):
-        print(f"state previous action of agent is {a}:")
-        print("actions are in this order [0,1,2,...,#actions]")
 
-        for k in range(num_actions*num_agents):
-            print(f"state cumulative actions {k}: {agent.qtable[a][k]}")
+
+for i, agent in enumerate(agents):
+    '''print()
+    print('Softmax values:')
+    print(agent.softmax())
+    print(f"Q-values for agent {i}:")
+    #print("Mean actions for Q-values are given for states")
+    print("actions are in this order [0,1,2,...,#actions]")
+
+    # num_actions only if we have memory true. Otherwise 0+1=1 amount of states for actions
+    for a in range((num_actions-1)*memory+1):
+        print('-----------------------------------------------')
+        print(f"state[0]: previous q_{i} = {a}:")
+        print()
+
+        for k in range(num_cumulative_action):
+            #bullshit, because the weights are negative:
+            #weights = [q_value / sum(agent.qtable[a][k]) for q_value in agent.qtable[a][k]]
+            #mean_action = sum(weight*action for weight, action in zip(weights,range(num_actions)))
+            #print(f"state[1]: mean for previous sum(q_i) = {k}: {mean_action}")
+            print(f"state[1]: previous sum(q_i) = {k}: {agent.qtable[a][k]}")'''
+
+print('---------------------------------------------------')
+NEquantity = round((demand_quantity-variable_cost)*num_agents/(demand_factor*(num_agents+1)))
+NEprofit = round((demand_quantity-variable_cost)**2*num_agents/(demand_factor*(num_agents+1)**2))
+print(f'Nash equilibrium amount is: {NEquantity} with profit {NEprofit}')
+COquantity = round((demand_quantity-variable_cost)/2/demand_factor)
+COprofit = round((demand_quantity-variable_cost)**2/4/demand_factor)
+print(f'Collusive equilibrium amount is: {COquantity} with profit {COprofit}')
+print()
+print('joint profits of the last 10 rounds:')
+print(joint_profits[-10:])
+print('joint quantities of the last 10 rounds:')
+print(joint_quantities[-10:])
+
+print(f"The mean joint profit of last 100 rounds: {np.mean(joint_profits[-10:])}")
+print(f"The mean joint quantity of last 100 rounds: {np.mean(joint_quantities[-10:])}")